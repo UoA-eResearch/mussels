5020abb158e5b5d1efa23aa516661c9cbb9c79a1
Nick Young <nick.young@auckland.ac.nz>
2023-10-05 15:19:59 +1300

    script to aggregate results

---
+++ aggregate_results.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from glob import glob
+import pandas as pd
+from tqdm.auto import tqdm
+
+files = glob("results/**/*.csv", recursive=True)
+print(len(files))
+
+results = []
+for f in tqdm(files):
+    result = pd.read_csv(f).length_cm.describe()
+    result["filename"] = f
+    results.append(result)
+
+df = pd.DataFrame(results)
+print(df)
+failed = df["count"] == 0
+print("Failures:")
+print(failed.value_counts())
+df.to_csv("results.csv")
